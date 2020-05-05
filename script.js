2c432b349059c6a033d56bad7fd694e3dfba1c93
Gabriel <gabriel.andrad4@gmail.com>
2020-05-05 17:21:39 -0300

    posição dos blocos aleatória

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-const buttons = document.querySelector("#buttons");
-var matriz = [[0,1,2], [3,4,5], [6,7,8]];
+const positions = ["00",
+                   "01",
+                   "02",
+                   "10",
+                   "11",
+                   "12",
+                   "20",
+                   "21",
+                   "22"];
 
+function random(list) {
+    const sorteado = true;
+    while (sorteado == true){
+        var rand = parseInt(Math.random() * 9);
+        if (list.includes(rand) == false){
+            return rand;
+        }
+    }
+}
+
+var array = [0,1,2,3,4,5,6,7,8];
+var list = [];
+
+for (var i = 0; i < 9; i++){
+    console.log("#pos" + positions[i]);
+    var rand = random(list);
+    console.log(rand);   
+    list.push(rand);
+}
+
+const blocks = document.querySelectorAll(".block");
+blocks.forEach((element, index) => {
+    element.id = "pos" + positions[list[index]];
+    console.log(list[index]);
+    
+});
+
+const buttons = document.querySelector("#buttons");
 buttons.addEventListener('click', event => {
     move(event.target.id);
 });
@@ -11,16 +46,10 @@ window.addEventListener('keyup', event => {
     
 });
 
-function move(direction) {
-    console.log(direction);
+function move(direction){
     const currentPosition = document.querySelector(".block0");
-    console.log(possibleMove(currentPosition, direction));
-}
-
-function possibleMove(currentPosition, direction){
     var row = currentPosition.id[currentPosition.id.length - 2];
     var column = currentPosition.id[currentPosition.id.length - 1];
-    console.log(row, column);
     switch (direction) {
         case "ArrowUp":
             if (row == "0") {
